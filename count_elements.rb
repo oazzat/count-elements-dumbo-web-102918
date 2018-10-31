c196675e74599a9d22c030fcdb67e51d03b4b75a
Omar A <oazzat19@gmail.com>
2018-10-31 16:02:33 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require "pry"
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  
+  array.each do |word|
+    if !new_hash.include?(word)
+      new_hash[word] = 1
+    else
+      new_hash[word] += 1
+    end
+  end
+  new_hash
 end
  
