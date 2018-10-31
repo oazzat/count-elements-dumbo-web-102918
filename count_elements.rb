e1b4d9ded0e29133939746097e10cd1ef771f4f6
Omar A <oazzat19@gmail.com>
2018-10-31 16:02:11 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -4,7 +4,11 @@ def count_elements(array)
   new_hash = {}
   
   array.each do |word|
-    if new_hash.include?
+    if !new_hash.include?(word)
+      new_hash[word] = 1
+    else
+      new_hash[word] += 1
+    end
   end
 end
  
